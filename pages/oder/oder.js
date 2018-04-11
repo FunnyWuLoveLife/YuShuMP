e31144fc027229c65a094830eb1870ec23e6cad6
Funnywu <agiot1026@163.com>
2018-04-11 20:45:02 +0800

    添加书单页

--- pages/oder/oder.js
+++ pages/oder/oder.js
@@ -1,30 +1,38 @@
 // pages/category/category.js
-
 var app = getApp()
+
 Page({
   data: {
+    loadingHidden: true,
+    hasSchollInfo: app.globalData.hasSchollInfo,
+    choose: ['学校', '专业', '年级']
   },
-  onLoad: function () {
-    console.log('onLoad')
+  onLoad: function (options) {
     var that = this
   },
   onShow: function () {
-    console.log('onShow')
+
   },
+  /* 点击搜索栏跳转到搜索页面 */
   wxSearchFn: function (e) {
     var that = this
     wx.navigateTo({
       url: '/pages/search/search',
     })
   },
+  /* 点击扫一扫，搜索图书 */
   sweep: function (e) {
     // 允许从相机和相册扫码
     wx.scanCode({
       success: (res) => {
         var isbn = res.result
-        // TODO完善扫码逻辑
-        console.log('isbn', isbn)
+        wx.navigateTo({
+          url: '/pages/book-detail/book-detail?isbn=' + isbn,
+        })
       }
     })
+  },
+  headerTab: function (e) {
+
   },
 })
